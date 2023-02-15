bef8f7db63ab68a057d21a1fadf8d52cd0c6bc50
Pascal Berger <pascal.berger@swissgrc.com>
2023-02-15 17:01:35 +0100

    Update azure-kubelogin to 0.0.26

--- azure-kubelogin/tools/chocolateyinstall.ps1
+++ azure-kubelogin/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # DO NOT CHANGE THESE MANUALLY. Use update.ps1
-$url64      = 'https://github.com/Azure/kubelogin/releases/download/v0.0.25/kubelogin-win-amd64.zip'
-$checksum64 = 'c0b6226dff2e916ba4371575f429620ac14cb1977a50a44057ad5d958ecd615d'
+$url64      = 'https://github.com/Azure/kubelogin/releases/download/v0.0.26/kubelogin-win-amd64.zip'
+$checksum64 = '34202afdc12d5272c99a1cf67b63bfd689f488494bbbd0b5d4661577da70e194'
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
