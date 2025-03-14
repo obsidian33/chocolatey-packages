37c1899209fe7e6a9bcc4e3fd8cf245ad5e72f1b
Marcus Yu <147451973+marcusyu-curative@users.noreply.github.com>
2025-03-13 22:48:33 -0700

    Setting releases URL back to latest

--- azure-kubelogin/update.ps1
+++ azure-kubelogin/update.ps1
@@ -8,7 +8,7 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-$releases = "https://github.com/Azure/kubelogin/releases/tag/v0.1.9"
+$releases = "https://github.com/Azure/kubelogin/releases/latest"
 
 function global:au_SearchReplace {
     @{
